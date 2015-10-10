2040ec958cdadfe61966b97d334c06836566a400
dezayasa <dezayasa@ufl.edu>
2015-10-10 15:34:45 -0400

    excruzme, but I have some css for youuuuuuu

---
+++ modules/workouts/server/routes/workouts.server.routes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function(app) {
+	var workouts = require('../controllers/workouts.server.controller');
+	var workoutsPolicy = require('../policies/workouts.server.policy');
+
+	// Workouts Routes
+	app.route('/api/workouts').all()
+		.get(workouts.list).all(workoutsPolicy.isAllowed)
+		.post(workouts.create);
+
+	app.route('/api/workouts/:workoutId').all(workoutsPolicy.isAllowed)
+		.get(workouts.read)
+		.put(workouts.update)
+		.delete(workouts.delete);
+
+	// Finish by binding the Workout middleware
+	app.param('workoutId', workouts.workoutByID);
+};
