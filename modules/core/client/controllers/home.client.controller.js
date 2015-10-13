d0d4770b74acdb23a7f42912f7dc78c92f568b93
juliochavez <chavez.julio@gmail.com>
2015-10-13 01:49:14 -0400

    Redirects the user to either the sign-in page or their user feed, depending on their authentication status.

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,18 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		$scope.redirect = function () {
+			// If the user is signed in, redirect them to their User Feed.
+	    if ($scope.authentication.user) {
+				window.location.href = "/user-feeds";
+			}
+			// Else, if the user is not signed in, redirect them to the sign-in page.
+			else {
+				window.location.href = "/authentication/signin";
+			}
+		};
+		// Trigger the
+		$scope.redirect();
 	}
 ]);
