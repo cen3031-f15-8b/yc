24ddd6d4f6d30f75810a6af3bce131da8006c0da
Khaled Hassan <khaled.hassan@gmail.com>
2015-10-13 10:40:09 -0400

    user-feeds: remove user feeds entry from menu bar

--- modules/user-feeds/client/config/user-feeds.client.config.js
+++ modules/user-feeds/client/config/user-feeds.client.config.js
@@ -3,23 +3,5 @@
 // Configuring the User feeds module
 angular.module('user-feeds').run(['Menus',
 	function(Menus) {
-		// Add the User feeds dropdown item
-		Menus.addMenuItem('topbar', {
-			title: 'User feeds',
-			state: 'user-feeds',
-			type: 'dropdown'
-		});
-
-		// Add the dropdown list item
-		Menus.addSubMenuItem('topbar', 'user-feeds', {
-			title: 'List User feeds',
-			state: 'user-feeds.list'
-		});
-
-		// Add the dropdown create item
-		Menus.addSubMenuItem('topbar', 'user-feeds', {
-			title: 'Create User feed',
-			state: 'user-feeds.create'
-		});
 	}
 ]);
