9e6f4bccb9ad8975e70e250099c55435099b3dc7
Khaled Hassan <khaled.hassan@gmail.com>
2015-10-13 10:35:08 -0400

    core: replace window.location with $location in home.client.controller

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,18 +1,18 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$location',
+	function($scope, Authentication, $location) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
 		$scope.redirect = function () {
 			// If the user is signed in, redirect them to their User Feed.
 	    if ($scope.authentication.user) {
-				window.location.href = "/user-feeds";
+				$location.url('/user-feeds');
 			}
 			// Else, if the user is not signed in, redirect them to the sign-in page.
 			else {
-				window.location.href = "/authentication/signin";
+				$location.url('/authentication/signin');
 			}
 		};
 		// Trigger the
