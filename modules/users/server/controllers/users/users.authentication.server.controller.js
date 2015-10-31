5800f90e15c5e1d207393f32fa612ce6999027f4
Khaled Hassan <khaled.hassan@gmail.com>
2015-10-30 20:15:20 -0400

    authentication: remove user login from signupChild

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -80,16 +80,8 @@ exports.signupChild = function(req, res) {
 				message: errorHandler.getErrorMessage(err)
 			});
 		} else {
-			// Remove sensitive data before login
-			user.password = undefined;
-			user.salt = undefined;
-
-			req.login(user, function(err) {
-				if (err) {
-					res.status(400).send(err);
-				} else {
-					res.json(user);
-				}
+			res.status(200).send({
+				message: 'Added a child!'
 			});
 		}
 	});
