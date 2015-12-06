f7a14885ebea522d3ee0513a5be3531566d48d20
Khaled Hassan <khaled.hassan@gmail.com>
2015-12-06 02:52:22 -0500

    testing remove seleniumAddress from protractor.conf.js as it is specified in the gruntfile

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,7 +4,7 @@
 
 // some configuration options came from <https://masteringmean.com/lessons/733-Configuring-Protractor-for-E2E-Testing>
 exports.config = {
-	seleniumAddress: 'http://localhost:4444/wd/hub',
+	//seleniumAddress: 'http://localhost:4444/wd/hub',
 	baseUrl: 'http://localhost:' + (process.env.PORT || '3001'),
 	specs: ['modules/*/tests/e2e/*.js']
 };
