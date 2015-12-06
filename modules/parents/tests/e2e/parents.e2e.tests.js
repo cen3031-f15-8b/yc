42def34be526114bd3171ac900442c91509de356
Khaled Hassan <khaled.hassan@gmail.com>
2015-12-06 01:32:34 -0500

    testing: begin parents e2e testing for adding children

--- modules/parents/tests/e2e/parents.e2e.tests.js
+++ modules/parents/tests/e2e/parents.e2e.tests.js
@@ -2,9 +2,33 @@
 
 describe('Parents E2E Tests:', function() {
 	describe('Test Parents page', function() {
-		xit('Should not include new Parents', function() {
-			browser.get('http://localhost:3000/#!/parents');
-			expect(element.all(by.repeater('parent in parents')).count()).toEqual(0);
+		it('Should be able to sign up as a parent and add a child', function() {
+			browser.get('/authentication/signup');
+			element(by.model('credentials.firstName')).sendKeys('Test');
+			element(by.model('credentials.lastName')).sendKeys('Parent');
+			element(by.model('credentials.email')).sendKeys('nobody@example.com');
+			element(by.model('credentials.username')).sendKeys('testparent');
+			element(by.model('credentials.password')).sendKeys('parent123!');
+			element(by.buttonText('Sign up')).click();
+
+			expect(element.all(by.repeater('child in children')).count()).toBe(0);
+
+
+			browser.get('/settings/addchild');
+			element(by.model('credentials.firstName')).sendKeys('Test');
+			element(by.model('credentials.lastName')).sendKeys('Child1');
+			element(by.model('credentials.age')).sendKeys('7');
+			element(by.model('credentials.username')).sendKeys('testparentchild1');
+			element(by.model('credentials.password')).sendKeys('parent123!');
+			element(by.buttonText('Add child')).click();
+			expect(element(by.binding('success')).getText()).toEqual('Added a child!');
+
+			browser.get('/');
+			expect(element.all(by.repeater('child in children')).count()).toBe(1);
+		});
+
+		afterEach(function(){
+			browser.get('/api/auth/signout');
 		});
 	});
 });
